0f2c87ec82427e5e8ffdbce03253f9d4f5a723de
damiiya <ddam2lee@gmail.com>
2022-06-06 19:52:17 +0900

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const callSomething = async () => {
+    let data = {
+      day: "일",
+      sleep_time: "10:00",
+    };
+    const response = await fetch("http://localhost:5001/sleep_times", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log(response);
+  };
+
+  React.useEffect(() => {
+    callSomething();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
