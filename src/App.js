16229eca95df0268af3ab8ad3ce240347b2c9153
damiiya <ddam2lee@gmail.com>
2022-06-06 21:40:59 +0900

    fetch & axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import axios from "axios";
 
 function App() {
   const callSomething = async () => {
@@ -19,8 +20,37 @@ function App() {
     console.log(response);
   };
 
+  // const callSomethingAxios = () => {
+  //   // json 타입으로 바꿔주지 않아도 axios가 알아서 바꿔줌
+  //   // [config 설정으로 사용하기]
+  //   axios({
+  //     method: "get",
+  //     url: "http://localhost:5001/sleep_times",
+  //   }).then((response) => {
+  //     console.log(response);
+  //   });
+
+  //   // [요청 메쏘드 사용하기]
+  //   axios.get("http://localhost:5001/sleep_times").then((response) => {
+  //     console.log(response);
+  //   });
+
+  //   // [작성 메쏘드 사용하기]
+  //   let data = {
+  //     day: "일",
+  //     sleep_time: "10:00",
+  //   };
+
+  //   axios.post("http://localhost:5001/sleep_times", data).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
+
   React.useEffect(() => {
-    callSomething();
+    // fetch 할 때 네이밍
+    // callSomething();
+    // axios 할 때 네이밍
+    // callSomethingAxios();
   }, []);
 
   return <div className="App"></div>;
